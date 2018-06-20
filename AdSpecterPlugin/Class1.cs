9519def55e5b7bc84b007e6d587212cf6ad1ab88
zhangerang <37486431+zhangerang@users.noreply.github.com>
2018-06-20 15:01:37 -0400

    Portal fetches ads from server

--- AdSpecterPlugin/Class1.cs
+++ AdSpecterPlugin/Class1.cs
@@ -196,7 +196,7 @@ namespace AdSpecter
             }
 
             var baseUrl = "https://adspecter-sandbox.herokuapp.com/ad_units/fetch";
-            //            var baseUrl = "http://localhost:3000/ad_units/fetch";
+           
             var appSession = AdSpecterConfigPlugIn.appSessionWrapper.app_session;
 
             var url = baseUrl +
@@ -206,6 +206,7 @@ namespace AdSpecter
                       "&app_session_id=" + appSession.id +
                       "&developer_app_id=" + appSession.developer_app_id;
 
+          //  url = "https://adspecter-sandbox.herokuapp.com/ad_units/fetch_portal?ad_format=image_360";
             UnityWebRequest uwr = UnityWebRequest.Get(url);
 
             yield return uwr.SendWebRequest();
@@ -239,6 +240,57 @@ namespace AdSpecter
             }
         }
 
+        public IEnumerator Get360AdUnit(GameObject adUnit, string format)
+        {
+            Debug.Log("started 360 mehotd, assigning " + adUnit);
+            ASRUAdUnit = adUnit;
+            Debug.Log("adunit: " + ASRUAdUnit + "old one " + adUnit);
+            var baseUrl = "https://adspecter-sandbox.herokuapp.com/ad_units/fetch_portal";
+
+            // var appSession = AdSpecterConfigPlugIn.appSessionWrapper.app_session;
+            //Debug.Log("Appsession: " + appSession);
+            var url = baseUrl +
+                    "?ad_format=" + format;
+            //            "&app_session_id=" + appSession.id +
+            //          "&developer_app_id=" + appSession.developer_app_id;
+           
+            Debug.Log("url" + url);
+
+            UnityWebRequest uwr = UnityWebRequest.Get(url);
+            Debug.Log("made uwr");
+            yield return uwr.SendWebRequest();
+            Debug.Log("sent web request");
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log("Error while retrieving ad: " + uwr.error);
+            }
+            else
+            {
+                adUnitWrapper = AdUnitWrapper.CreateFromJSON(uwr.downloadHandler.text);
+                Debug.Log("wrpaper: " + adUnitWrapper);
+                impressionId = adUnitWrapper.impression_id;
+                
+                Debug.Log("IMPRESSION ID: " + impressionId);
+
+                switch (format)
+                {
+                    case "image_360":
+                        {
+                            Debug.Log("getting: " + adUnitWrapper.ad_unit.ad_unit_url);
+                            StartCoroutine(GetImageTexture(adUnitWrapper.ad_unit.ad_unit_url));
+                            break;
+                        }
+
+                    case "video_360":
+                        {
+                            GetVideo(adUnitWrapper.ad_unit.ad_unit_url);
+                            //GetVideo("https://www.quirksmode.org/html5/videos/big_buck_bunny.mp4");
+                            break;
+                        }
+                }
+            }
+        }
+
         void GetVideo(string url)
         {
             video = ASRUAdUnit.AddComponent<UnityEngine.Video.VideoPlayer>();
@@ -253,9 +305,9 @@ namespace AdSpecter
         IEnumerator GetImageTexture(string url)
         {
             UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
-
+            Debug.Log("url: " + url);
             yield return www.SendWebRequest();
-
+            Debug.Log("sent web request");
             if (www.isNetworkError || www.isHttpError)
             {
                 Debug.Log("Error while getting ad texture:" + www.error);
@@ -263,7 +315,7 @@ namespace AdSpecter
             else
             {
                 Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
-
+                Debug.Log("texture: " + myTexture);
                 ASRUAdUnit.GetComponent<Renderer>().material.mainTexture = myTexture;
 
                 hasAdLoaded = true;
@@ -559,7 +611,7 @@ namespace AdSpecter
 
         public IEnumerator GetGeoData()
         {
-            UnityWebRequest www = UnityWebRequest.Get("https://api.ipdata.co");
+            UnityWebRequest www = UnityWebRequest.Get("https://api.ipdata.co/?api-key=1ad68e416ab80c9eaa32d01f42de56210053fff2bb3c0d838af50e34");
             yield return www.SendWebRequest();
 
             if (www.isNetworkError || www.isHttpError)
